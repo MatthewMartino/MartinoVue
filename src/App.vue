23f25e0636b6d51490684c025f15a8b690581ed1
MatthewMartino <matt.martino@hey.com>
2023-01-22 10:52:38 -0800

    More bug fixes, navbar is now working correctly

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
 import { RouterView } from "vue-router";
-import customNav from "./components/customNav.vue"
+import customNav from "./components/customNav.vue";
+
+export default {
+  components: {
+    customNav,
+  },
+};
 </script>
 
 <template>
-  <header>
-    <customNav />
-  </header>
+<header>
+  <customNav />
+</header>
 
   <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-
 import "./assets/scss/main.scss";
 
 const app = createApp(App);
