7cd0b503f74a2b1e4e66af6341f78a4ac11b03ee
MatthewMartino <matt.martino@hey.com>
2023-01-23 11:09:15 -0800

    Mobile menu seems functional

--- src/components/customNav.vue
+++ src/components/customNav.vue
@@ -5,8 +5,15 @@ export default {
       showI: false,
       showT: false,
       showYT: false,
+      menuHidden: true,
     };
   },
+  methods: {
+    toggleMobileMenu() {
+      this.menuHidden = !this.menuHidden
+      console.log("menuHidden = " + this.menuHidden)
+    }
+  }
 };
 </script>
 
@@ -14,15 +21,21 @@ export default {
   <div class="customNav">
     <div class="navTitle">martino hq</div>
 
-    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#212529" viewBox="0 0 16 16" class="menuIcon">
-      <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-    </svg>
+    <svg @click="toggleMobileMenu" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#212529" viewBox="0 0 16 16" class="menuIcon"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/></svg>
 
-    <div class="navLink"><RouterLink to="/">🏠 home</RouterLink></div>
-    <div class="navLink"><RouterLink to="/about">📚 about</RouterLink></div>
-    <div class="navLink"><RouterLink to="/contact">👋 contact</RouterLink></div>
-    <div class="navLink no">
-      <RouterLink to="#">🌍 store (soon!)</RouterLink>
+    <div class="mobileMenuToggle" v-if="menuHidden">
+      <div class="navLink">
+        <RouterLink to="/">🏠 home</RouterLink>
+      </div>
+      <div class="navLink">
+        <RouterLink to="/about">📚 about</RouterLink>
+      </div>
+      <div class="navLink">
+        <RouterLink to="/contact">👋 contact</RouterLink>
+      </div>
+      <div class="navLink">
+        <RouterLink to="#">🌍 store (soon!)</RouterLink>
+      </div>
     </div>
 
     <a
